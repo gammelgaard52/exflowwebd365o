0aa58aeb42370aa8e0f803fedc7d8797a0a19957
Martin Rasmussen <gammelgaard_mink@msn.com>
2017-11-22 10:49:45 +0100

    Removed some text

--- App-RegistrationDeployment.ps1
+++ App-RegistrationDeployment.ps1
@@ -31,17 +31,7 @@ param(
     [string]$ReplacePattern='[^a-zA-Z0-9]'
 
 )
-<#
-$Location                  = "northeurope" #Azure location notheurope, westeurope,... 
-$Security_Admins           = "EGMAGRA,KEFOL,MBRAN,KIJNI" #AX user name (UPPERCASE) of ExFlow web administrators. Admins can translate texts, write welecome messages, ...
-$DynamicsAXApiId           = "coro-uat.sandbox.operations.dynamics.com/" #URL such as axtestdynamics365aos.cloudax.dynamics.com
-$ExFlowUserSecret          = "1a73f90b52c44c70a892987cb526d263" #Your identity recieved by signupsoftware.com
-$Prefix                    = "" #Optional prefix (short using alphanumeric characters). Leave blank for default behavior.
-$PackageVersion            = "" #Optional version to install.  Leave blank for default behavior.
-$TenantGuid                = "2d9c7aaa-2448-49d8-ad14-9044694cbaaf" #Optional tenant id when you have multiple tenants (advanced).   
-$WebAppSubscriptionGuid    = "4bb56e2f-3830-4c3f-9bd5-8578403dd03c" #Optional Subscription for the web app (advanced). Use if you have two subscriptions, one holding tenant (AD) and another for apps. You will be prompted twice for cretedials, (1) use AD admin credentials, (2) the subscription co-admin for the second subscription.       
-$TenantnameSpecific        = "corocorp.onmicrosoft.com" #Specify the Azure AD tenant name.
-#>
+
 #Function to get authorization token for communication with the Microsoft Graph REST API
 Function GetAuthorizationToken
 {
@@ -215,8 +205,6 @@ If ($HasErrors){
 
 
 #region Determine AzureRmDnsAvailability
-write-host "MAGRA"
-write-host $ReplacePattern
 $_TenantId = $DynamicsAXApiId.split('.')[0].ToLower('')
 if ($_TenantId.Length -gt 16)
 {
